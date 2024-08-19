69128f3b6a5b0fcb62e34dfa5bfbaf34fc4199b5
Johan Pham <johanpham2711@gmail.com>
2024-08-19 15:23:21 +0700

    fix: update user module

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
 
-  async getUser(): Promise<any> {
-    return this.usersRepository.delete({
-      id: 'clzv4nvbi0000lmni5tq6cdrk',
-    });
+  async getUsers(): Promise<User[]> {
+    return this.usersRepository.findMany();
   }
 }
